1fb31c8186561beded99d20f3c672f497ad1c572
Josh Burri <burrijw@gmail.com>
2023-05-04 12:04:15 -0600

    Some small style edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./**/*.js"],
+  content: ["./index.html", "./*.js"],
   theme: {
     colors: {
       transparent: "transparent",
